e4f78979c854f1f9a216452a31a8b3405b0788e9
kenneth2947 <39242086+kenneth2947@users.noreply.github.com>
2018-05-13 14:27:31 -0500

    Add files via upload

---
+++ arbol_bueno_fin.ino
@@ -0,0 +1,33 @@
+
+char phone_no[]="+50244521157";
+int led=2;
+void setup() {
+  pinMode(led,INPUT);
+  Serial.begin (9600);
+}
+void loop()
+{
+  
+  if(digitalRead (led)== HIGH)
+
+{
+ Serial.begin(9600);
+delay(300); 
+
+Serial.println("AT+CMGF=1");    
+delay(2000);
+Serial.print("AT+CMGS=\"");
+Serial.print(phone_no); 
+Serial.write(0x22);
+Serial.write(0x0D);  // hex equivalent of Carraige return    
+Serial.write(0x0A);  // hex equivalent of newline
+delay(2000);
+Serial.print("La seccion C del bosque, esta en posible amenaza");
+delay(500);
+Serial.println (char(26));//the ASCII code of the ctrl+z is 26
+} 
+}
+
+  
+
+
